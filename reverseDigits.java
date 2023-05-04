398c18b86402f9c62ae7ab6c51aeb65ddae9b2a3
rirefat <rirefat.official@gmail.com>
2023-05-05 04:44:05 +0600

    add reverse number problem

--- reverseDigits.java
+++ reverseDigits.java
@@ -1,17 +1,19 @@
-// Ask user to give input and calculate the sum of all the digits
+// Take positive input from user and reverse it.
 
 import java.util.Scanner;
-public class sumOfDigits {
+
+public class reverseDigits {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter positive digits: ");
+        System.out.print("Input Positive number: ");
         int num = input.nextInt();
-        int sum = 0;
         int temp = num;
+        int sum = 0;
+        int r;
 
         while (temp != 0){
-            int remainder = temp%10;
-            sum +=remainder;
+            r = temp%10;
+            sum = sum*10+r;
             temp = temp/10;
         }
         System.out.println("Result: "+sum);
