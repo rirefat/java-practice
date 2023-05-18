8bad44366ab467cb18f80a57e27be922793fb199
rirefat <rirefat.official@gmail.com>
2023-05-18 11:53:14 +0600

    add counting prime numbers

--- LeetCode_problem_solving/Count_Primes.java
+++ LeetCode_problem_solving/Count_Primes.java
@@ -1,18 +1,17 @@
 package LeetCode_problem_solving;
 
-public class test {
+public class Count_Primes {
     public static void main(String[] args) {
         int n = 10;
-        int count = 0, primeNumbers = 0;
-
-        for (int i = 2; i<n; i++){
-            for (int j = 2; j<i; j++){
-                if(i%j == 0){
+        int count = 0, primeNumbers=0;
+        for (int i=2; i<=n; i++){
+            for (int j=2; j<=i/2; j++){
+                if (i%j == 0){
                     count++;
                     break;
                 }
             }
-            if (count == 0){
+            if (count==0){
                 primeNumbers++;
             }
             count=0;
