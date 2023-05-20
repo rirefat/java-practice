e62ce48687831c52ea9a945fd9b82d2861c4ab24
rirefat <rirefat.official@gmail.com>
2023-05-20 10:51:12 +0600

    add variable types

---
+++ VariableTypes.java
@@ -0,0 +1,15 @@
+public class VariableTypes {
+    // Instance variables
+    int age;
+    int name;
+
+    // Static variables
+    static String universityName;
+
+    // loval variables are used in the method below
+    void display(int age, String name){
+        System.out.println("Age: "+age);
+        System.out.println("Name: "+name);
+    }
+}
+
