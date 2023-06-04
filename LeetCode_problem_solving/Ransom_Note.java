121a82f399bd18d05eabcaa1e12f2bd52f550c25
rirefat <rirefat.official@gmail.com>
2023-06-05 03:23:57 +0600

    add leetcode problem titled ransome notes

--- LeetCode_problem_solving/Ransom_Note.java
+++ LeetCode_problem_solving/Ransom_Note.java
@@ -5,12 +5,22 @@ import java.util.Collections;
 
 public class Ransom_Note {
     public static void main(String[] args) {
-        String ransomNote = "aa", magazine = "refat";
-        int[] count = new int[26];
-        for(char ch: magazine.toCharArray()) {
-            System.out.println(count[ch - 'a']++);
-//            System.out.println(count[ch - 'a']++);
+        String ransomNote = "baa", magazine = "aab";
 
+        boolean response = true;
+        if (ransomNote.length() > magazine.length()) response = false;
+        int[] alphabets = new int[26];
+
+        for(char c:magazine.toCharArray()){
+            alphabets[c-'a']++;
         }
+        for(char c:ransomNote.toCharArray()){
+            if( alphabets[c-'a']==0 ) {
+                response = false;
+                break;
+            }
+            alphabets[c-'a']--;
+        }
+        System.out.println(response);
     }
 }
